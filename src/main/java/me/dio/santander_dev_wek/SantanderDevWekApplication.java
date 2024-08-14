e486a672726b9785fc452f4ecffc563edb69ee21
Robert Dutra <robertdutra10@hotmail.com>
2024-08-14 13:58:53 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_dev_wek/SantanderDevWekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_wek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWekApplication.class, args);
+	}
+
+}
